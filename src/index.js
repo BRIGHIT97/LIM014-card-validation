70a142a9a97e5a719d6f931b6e9a0191fc58473d
BRIGHIT97 <brighit.qa@gmail.com>
2021-01-26 21:55:27 -0500

    agregar una alerta para comprobar campos vacios y dar formato a la respuesta de validación de la tarjeta

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 import validator from './validator.js';
 
-console.log(validator);
+/*console.log(validator);*/
+  
+document.getElementById("validar").addEventListener("click", validarCard); 
+
+
+function validarCard(event){
+    event.preventDefault();
+ 
+    var userNumInput = document.getElementById("cardNumber").value;
+    var expiry = document.getElementById("expiry").value;
+    var cardCode = document.getElementById("cardCode").value;
+        
+    
+    if ((userNumInput == "") || (expiry== "") ||(cardCode == "") ) {  //COMPRUEBA CAMPOS VACIOS
+        alert("Los campos no pueden quedar vacios");
+        return false;
+    }
+
+    let tarjetaValida = validator.isValid(userNumInput);
+    let enmascarado = validator.maskify(userNumInput); 
+    
+   
+    if (tarjetaValida == true){
+    
+        document.getElementById("secondContainer").innerHTML = "La tarjeta " + enmascarado + " es válida" ;
+        
+    }
+    else {
+    
+        document.getElementById("secondContainer").innerHTML = "La tarjeta " + enmascarado + " es inválida";
+         
+        
+
+    }
+
+}
+
+
